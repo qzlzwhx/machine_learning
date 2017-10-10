feef8f9f39c08136cf3c3b35a78bc5b954d70773
qzlzwhx <qiaozhanlei@baofeng.com>
2017-10-10 16:07:54 +0800

    第五章暂告一段落

--- logRegres.py
+++ logRegres.py
@@ -1,4 +1,11 @@
 # coding:utf8
+"""
+逻辑回归的主要思想就是，根据现有数据对分类边界线建立回归公式，以此为分类。训练分类器的过程就是寻找最佳拟合参数的过程。
+sigmoid函数，单位阶跃函数的一种。这种函数在x=0的时候，sigmoid结果是0.5，随着x无限增大，sigmoid越来越接近与1，想法无限减小的时候在逐渐
+接近0。而逻辑回归算法就是为了寻找这一样一个最佳系数，sigmoid的输入z由如下公式得到，z=wx,w是一个列向量，就是我们寻找的回归系数，
+实际上他就是权重，每个特征的权重，x就是输入数据，一个一个的元素。
+所以寻找回归系数的过程就是，计算每一个输入单元，对权重的影响，最终得到你想要的权重值，也就是回归系数。
+"""
 
 from numpy import *
 
@@ -30,10 +37,14 @@ def gradAscent(data_array, class_labels):
 	weights = ones((n, 1))
 	# 矩阵乘，由左边的矩阵绝对行，右边的矩阵绝对列
 	# 所谓，乘法结果，第m行n列那个元素就是左边矩阵第m行，和右边矩阵第n列挨个相乘之和所得
-	# 所谓
+	# 注意和下边随机梯度上升算法的区别，
 	for k in range(max_cycles):
+		# 计算整个矩阵所有元素的sigmoid值，也就是100*3的矩阵和3*1的矩阵相乘，得到100*1的列向量
+		# 这个h就是对矩阵元素的预测，因为预测结果就是根据sigmoid来的吗
 		h = sigmoid(data_matrix * weights)
+		# 两个列向量减法，这里的error的意思就是真是标签和预测标签的差值
 		error = class_labels - h
+		# 计算整个矩阵对权重的影响，也就是回归系数。因为error是一个100*1的向量，所以需要对矩阵进行转置3*100
 		weights = weights + alpha * data_matrix.transpose() * error
 	return weights
 
@@ -67,8 +78,11 @@ def stocGradAscent0(data_mat, class_labels):
 	alpha = 0.01
 	weights = ones(column)
 	for i in range(row):
+		# 随机梯度上升，这里计算是挨个元素计算，h的意义就是下表是i的那个元素，的sigmoid值，也就是它的预测分类
 		h = sigmoid(sum(data_mat[i] * weights))
+		# 真是分类和预测分类的差值
 		error = class_labels[i] - h
+		# 权重，计算，该元素对权重带来的影响
 		weights = weights + alpha * data_mat[i] * error
 	return weights
 
@@ -90,4 +104,42 @@ def stocGradAscent1(dataMat, labels, times=150):
 	return weights
 
 
+def classifyVector(intX, weights):
+	prob = sigmoid(sum(intX*weights))
+	if prob > 0.5:
+		return 1
+	else:
+		return 0
 
+
+def colicTest():
+	training_file = open('./Ch05/horseColicTraining.txt')
+	test_file = open('./Ch05/horseColicTest.txt')
+	data_list = []
+	labels = []
+	for line in training_file.readlines():
+		attrs = line.strip().split('\t')
+		data_list.append(map(float, attrs[:20]))
+		labels.append(float(attrs[-1]))
+	# print data_list
+	weights = stocGradAscent1(array(data_list), labels, 500)
+	error_count = 0
+	num_test = 0
+	for line in test_file.readlines():
+		num_test += 1
+		atrrs = line.strip().split('\t') 
+		real_label = int(atrrs[21])
+		if classifyVector(map(float, attrs[:20]), weights) != real_label:
+			error_count += 1
+	return error_count, num_test, weights
+			
+
+def multitest(times=10):
+	
+	for i in range(10):
+		a, b,c = colicTest()
+		print a, b, c
+			
+
+		
+		
